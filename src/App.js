61725d20f99bad400abf59045576d5d7643b2a77
sungwon.cha <mygenie3@naver.com>
2019-01-16 01:42:45 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import logo from './logo.svg';
+import template from './App.pug';
+
+class App extends Component {
+  render() {
+    return template({
+      // variables
+      logo,
+    });
+  }
+}
+
+export default App;
