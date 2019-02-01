99fa4715979c206c8824e77ec697110002caa3f8
sungwon.cha <mygenie3@naver.com>
2019-02-01 13:54:04 +0900

    usage: rollup

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,13 @@
 import { Component } from 'react';
 import logo from './logo.svg';
 import template from './App.pug';
+import { greeting } from './core';
 
 class App extends Component {
   render() {
     return template({
       // variables
+      greeting: greeting('Emmet'),
       logo,
     });
   }
